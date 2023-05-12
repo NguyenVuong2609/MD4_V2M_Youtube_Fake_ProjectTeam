851db49c445f7c0cf62c37923eb1a1cf6342139e
vangtt <giangthanhvan12@gmail.com>
2023-05-12 11:12:58 +0700

    12-05 created Role class

---
+++ src/main/java/rikkei/academy/model/Role.java
@@ -0,0 +1,30 @@
+package rikkei.academy.model;
+
+public class Role {
+    int id;
+    RoleName name;
+
+    public Role() {
+    }
+
+    public Role(int id, RoleName name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RoleName getName() {
+        return name;
+    }
+
+    public void setName(RoleName name) {
+        this.name = name;
+    }
+}
