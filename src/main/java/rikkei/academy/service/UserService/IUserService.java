74613e1925e97c4c98356ee56be1abac087f4964
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2023-05-15 10:42:43 +0700

    1505 update folder Service

---
+++ src/main/java/rikkei/academy/service/UserService/IUserService.java
@@ -0,0 +1,7 @@
+package rikkei.academy.service.UserService;
+
+import rikkei.academy.model.User;
+import rikkei.academy.service.IGeneric;
+
+public interface IUserService extends IGeneric<User> {
+}
---
+++ src/main/java/rikkei/academy/service/UserService/UserServiceIMPL.java
@@ -0,0 +1,27 @@
+package rikkei.academy.service.UserService;
+
+import rikkei.academy.model.User;
+
+import java.util.List;
+
+public class UserServiceIMPL implements IUserService{
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public User findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+}
